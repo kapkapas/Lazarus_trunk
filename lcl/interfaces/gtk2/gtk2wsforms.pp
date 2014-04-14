kapkapas/Lazarus_trunk

commit 87116cb18e7e72cfab089616f283724567acec3b

Gtk2: fixed showing of popup window over fsStayOnTop forms.issue #26018
git-svn-id: https://svn.freepascal.org/svn/lazarus/trunk@44723 4005530d-fff6-0310-9dd1-cebe43e6787f

lcl/interfaces/gtk2/gtk2wsforms.pp
@@ -675,6 +675,9 @@ end;
 
 class procedure TGtk2WSCustomForm.ShowHide(const AWinControl: TWinControl);
 var
+  {$IFDEF HASX}
+  TempGdkWindow: PGdkWindow;
+  {$ENDIF}
   AForm: TCustomForm;
   GtkWindow: PGtkWindow;
   Geometry: TGdkGeometry;
@@ -741,6 +744,27 @@ begin
       gtk_window_set_transient_for(GtkWindow, nil); //untransient
       gtk_window_set_modal(GtkWindow, False);
     end;
+
+    {$IFDEF HASX}
+    // issue #26018
+    if AWinControl.HandleObjectShouldBeVisible and
+      not (csDesigning in AForm.ComponentState) and
+      not (TCustomForm(AWinControl).FormStyle in fsAllStayOnTop) and
+      not (fsModal in TCustomForm(AWinControl).FormState) and
+      (TCustomForm(AWinControl).PopupMode = pmAuto) and
+      (TCustomForm(AWinControl).BorderStyle = bsNone) and
+      (TCustomForm(AWinControl).PopupParent = nil) then
+    begin
+      TempGdkWindow := PGdkWindow(Gtk2WidgetSet.GetForegroundWindow);
+      if (TempGdkWindow <> nil) then
+      begin
+        if ((gdk_window_get_state(TempGdkWindow) and GDK_WINDOW_STATE_ABOVE) = GDK_WINDOW_STATE_ABOVE) or
+          GTK2WidgetSet.GetAlwaysOnTopX11(TempGdkWindow) then
+            gtk_window_set_keep_above(GtkWindow, True);
+      end;
+    end;
+    {$ENDIF}
+
     Gtk2WidgetSet.SetVisible(AWinControl, AForm.HandleObjectShouldBeVisible);
   end;
 

lcl/interfaces/gtk2/gtk2int.pas
@@ -298,7 +298,7 @@ type
     function GetWindowManager: String;
     function IsCurrentDesktop(AWindow: PGdkWindow): Boolean;
     function X11GetActiveWindow: HWND;
-
+    function GetAlwaysOnTopX11(AWindow: PGdkWindow): boolean;
     procedure HideAllHints;
     procedure RestoreAllHints;
     {$ENDIF}

lcl/interfaces/gtk2/gtk2widgetset.inc
@@ -4459,6 +4459,55 @@ begin
   if Valid then Result := {%H-}HWND(gdk_window_foreign_new(ResultWindow));
 end;
 
+function TGtk2WidgetSet.GetAlwaysOnTopX11(AWindow: PGdkWindow): boolean;
+var
+  Display: PDisplay;
+  X11Window: TWindow;
+  WMAtom: TAtom;
+  typeReturned: TAtom;
+  formatReturned: Integer;
+  nitemsReturned: PtrInt;
+  unused: PtrInt;
+  data: Pointer;
+begin
+  Result := False;
+  Display := gdk_x11_get_default_xdisplay;
+  if Display = nil then
+    exit;
+  X11Window := gdk_x11_drawable_get_xid(PGdkDrawable(AWindow));
+  if X11Window = 0 then
+    exit;
+  WMAtom := XInternAtom(Display,'_NET_WM_STATE', False);
+  if WMAtom > 0 then
+  begin
+    data := nil;
+    if XGetWindowProperty(Display, X11Window, WMAtom, 0, 1024, False, XA_ATOM,
+      @typeReturned, @formatReturned, @nitemsReturned,
+      @unused, @data) = Success then
+    begin
+      if (typeReturned = XA_ATOM) and (formatReturned = 32) and
+        (Data <> nil) then
+      begin
+        while nitemsReturned > 0 do
+        begin
+          // make happy ancient x11 or old kde ?
+          if XInternAtom(Display,'_NET_WM_STATE_STAYS_ON_TOP', False) = TAtom(Data^) then
+            Result := True
+          else
+          if XInternAtom(Display,'_NET_WM_STATE_ABOVE', False) = TAtom(Data^) then
+            Result := True;
+          dec(nItemsReturned);
+          if Result or (nItemsReturned = 0) then
+            break;
+          inc(Data);
+        end;
+        XFree(Data);
+        Data := nil;
+      end;
+    end;
+  end;
+end;
+
 procedure TGtk2WidgetSet.HideAllHints;
 var
   TopList, List: PGList;

commit 027997847e3fcb23e918e38483467ae82387e300

Qt,Gtk2: fixed crash introduced with r44723,44724 when popup window is shown above modal form. issue #26018
git-svn-id: https://svn.freepascal.org/svn/lazarus/trunk@44731 4005530d-fff6-0310-9dd1-cebe43e6787f

lcl/interfaces/gtk2/gtk2wsforms.pp
@@ -756,7 +756,7 @@ begin
       (TCustomForm(AWinControl).PopupParent = nil) then
     begin
       TempGdkWindow := PGdkWindow(Gtk2WidgetSet.GetForegroundWindow);
-      if (TempGdkWindow <> nil) then
+      if (TempGdkWindow <> nil) and (GdkWindowObject_modal_hint(GDK_WINDOW_OBJECT(TempGdkWindow)^) = 0) then
       begin
         if ((gdk_window_get_state(TempGdkWindow) and GDK_WINDOW_STATE_ABOVE) = GDK_WINDOW_STATE_ABOVE) or
           GTK2WidgetSet.GetAlwaysOnTopX11(TempGdkWindow) then

lcl/interfaces/qt/qtwsforms.pp
@@ -563,7 +563,7 @@ begin
         (TCustomForm(AWinControl).PopupParent = nil) then
       begin
         W := QApplication_activeWindow;
-        if W <> nil then
+        if (W <> nil) and (W <> QApplication_activeModalWidget) then
         begin
           Flags := QWidget_windowFlags(W);
           if (Flags and QtWindowStaysOnTopHint <> QtWindowStaysOnTopHint) and