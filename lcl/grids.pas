hovadur/lazarus

commit 01ac694c36b8026eba5c3b964cd44f585fe394b2

LCL: TCustomGrid: fixed range check error when scrolling editor control
git-svn-id: http://svn.freepascal.org/svn/lazarus/branches/fixes_1_2@44870 4005530d-fff6-0310-9dd1-cebe43e6787f

lcl/grids.pas
@@ -7601,13 +7601,20 @@ begin
     begin
       // editor is not in visible area, hide it complety
       // to avoid showing it in fixed cell area
-      with msg.CellRect do
-        Msg.CellRect := Rect(-Right, -Bottom, -Left, -Top);
+      Msg.CellRect := Bounds(-FEditor.Width-10, -FEditor.Height-10, FEditor.Width,FEditor.Height);
     end;
     if FEditorOptions and EO_AUTOSIZE = EO_AUTOSIZE then begin
       if EditorBorderStyle = bsNone then
           InflateRect(Msg.CellRect, -1, -1);
-      FEditor.BoundsRect := Msg.CellRect;
+      // hide editor if out of visible area
+      if (Msg.CellRect.Right>=0) and (Msg.CellRect.Bottom>=0)
+        and (Msg.CellRect.Left<=ClientWidth) and (Msg.CellRect.Top<=ClientHeight)
+      then begin
+        FEditor.Visible := true;
+        FEditor.BoundsRect := Msg.CellRect;
+      end else begin
+        FEditor.Visible := false;
+      end;
     end else begin
       Msg.LclMsg.msg:=GM_SETBOUNDS;
       Msg.Grid:=Self;

kapkapas/Lazarus_trunk

commit 578327bc0b3d14e9185eb72469cef98bf82e2fc5

LCL, grids, fix range check error on some GTK2 (fix also editor appearing regression introduced in r44870)
git-svn-id: https://svn.freepascal.org/svn/lazarus/trunk@45117 4005530d-fff6-0310-9dd1-cebe43e6787f

lcl/grids.pas
@@ -7618,26 +7618,20 @@ begin
 
     // send editor bounds
     Msg.CellRect:=CellRect(FCol,FRow);
-    if (Msg.CellRect.Top<FGCache.FixedHeight) or
-       (UseRightToLeftAlignment and (Msg.CellRect.Right-1>FlipX(FGCache.FixedWidth))) or
-       (not UseRightToLeftAlignment and (Msg.CellRect.Left<FGCache.FixedWidth)) then
-    begin
-      // editor is not in visible area, hide it complety
-      // to avoid showing it in fixed cell area
-      Msg.CellRect := Bounds(-FEditor.Width-10, -FEditor.Height-10, FEditor.Width,FEditor.Height);
-    end;
+
+    with msg.CellRect do
+    if (Top<FGCache.FixedHeight) or (Top>FGCache.ClientHeight) or
+       (UseRightToLeftAlignment and ((Right-1>FlipX(FGCache.FixedWidth)) or (Right<0))) or
+       (not UseRightToLeftAlignment and ((Left<FGCache.FixedWidth) or (Left>FGCache.ClientWidth))) then
+      // if editor will be out of sight, make the out of sight coords fixed
+      // this should avoid range check errors on widgetsets that can't handle
+      // high control coords (like GTK2)
+      Msg.CellRect := Bounds(-FEditor.Width-100, -FEditor.Height-100, Right-Left, Bottom-Top);
+
     if FEditorOptions and EO_AUTOSIZE = EO_AUTOSIZE then begin
       if EditorBorderStyle = bsNone then
-          InflateRect(Msg.CellRect, -1, -1);
-      // hide editor if out of visible area
-      if (Msg.CellRect.Right>=0) and (Msg.CellRect.Bottom>=0)
-        and (Msg.CellRect.Left<=ClientWidth) and (Msg.CellRect.Top<=ClientHeight)
-      then begin
-        FEditor.Visible := true;
-        FEditor.BoundsRect := Msg.CellRect;
-      end else begin
-        FEditor.Visible := false;
-      end;
+        InflateRect(Msg.CellRect, -1, -1);
+      FEditor.BoundsRect := Msg.CellRect;
     end else begin
       Msg.LclMsg.msg:=GM_SETBOUNDS;
       Msg.Grid:=Self;